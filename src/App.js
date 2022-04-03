5afdc9563976ff33c63cbbb86cd91b9b78361c5b
Shubham Shrikant Bawner <wurtzreaxnwittigreaxn@gmail.com>
2022-04-03 16:15:10 +0530

    initial upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,195 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Body from './components/Body';
+import { useState } from 'react'
+
+const allItems = []
+
+function App(props) {
+    let action = {}
+    let [items, setItems] = useState(allItems)
+    let cart = []
+    let allAvailableItems = []
+
+    let itemLoader = {}
+
+    let addToCart = {}
+    addToCart.name = "Add to Cart"
+    addToCart.action = (item) => {
+        if (checkToken()) {
+            cart.push(item)
+            localStorage.setItem('cart', JSON.stringify(cart))
+        }
+        else props.auth()
+        console.log(cart)
+    }
+    let removeFromCart = {}
+    removeFromCart.name = "Remove"
+    removeFromCart.action = (item) => {
+        if (checkToken()) {
+            let cart = JSON.parse(localStorage.getItem('cart'))
+            let temp = cart.length
+            cart = cart.filter((i) => { return JSON.stringify(item) != JSON.stringify(i) })
+            if (cart.length == temp) window.alert(`item ${JSON.stringify(item)} could not be removed!`)
+            else {
+                localStorage.setItem('cart', JSON.stringify(cart))
+                let tempCart = [...cart, removeFromCart]
+                setItems(tempCart);
+            }
+        }
+        else props.auth()
+        console.log(cart)
+    }
+    let cancellOrder = {}
+    cancellOrder.name = "Cancell Order"
+    cancellOrder.action = (item) => {
+        let authToken = checkToken()
+        if (!authToken) props.auth()
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Authorization", authToken);
+
+        var raw = JSON.stringify({
+            "orderId": item.orderId
+        });
+
+        var requestOptions = {
+            method: 'DELETE',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch("https://e-commerce.urownsite.xyz/orders/cancel", requestOptions)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response, 'cansell req ka response');
+                if (response["status"] == "Success") {
+                    window.alert("order cancelled successfully")
+                    itemLoader.loadHistory();
+                }
+                else {
+                    props.auth()
+                }
+            }).catch(error => console.log('error', error));
+    }
+
+    let placeOneOrder = {}
+    placeOneOrder.name = "Order Item"
+    placeOneOrder.action = async function (item) {
+        let authToken = checkToken()
+        if (!authToken) props.auth()
+        
+    }
+
+    itemLoader.loadAllItems = async function () {
+        if (allAvailableItems.length > 0) setItems(allAvailableItems);
+        else {
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+            // var raw = "";
+
+            var requestOptions = {
+                method: 'GET',
+                headers: myHeaders,
+                // body: raw,
+                redirect: 'follow'
+            };
+
+            await fetch("https://e-commerce.urownsite.xyz/products", requestOptions)
+                .then(allItems => allItems.json())
+                .then(allItems => {
+                    console.log(allItems);
+                    allAvailableItems = allItems.data
+                    allAvailableItems.push(addToCart)
+                    setItems(allAvailableItems);
+                })
+
+            console.log('aaaaaaaaaa')
+        }
+    }
+
+
+    itemLoader.loadCart = async function () {
+        if (!checkToken()) props.auth()
+        let cart = JSON.parse(localStorage.getItem('cart'))
+        let tempCart = [...cart, removeFromCart]
+        console.log(tempCart, 'lll')
+        if (cart.length > 1) {
+            setItems(tempCart);
+        }
+        else window.alert('cart is empty!')
+    }
+
+    itemLoader.placeOrders = async function () {
+        if (!checkToken()) props.auth()
+        let cart = JSON.parse(localStorage.getItem('cart'))
+        if (cart.length > 1) {
+            let status = "Failed";
+            for (let i = 0; i < cart.length; i++) {
+                status = await placeOneOrder.action(cart[i])
+            }
+            if (status == "Failed") window.alert(`no order placed!`)
+            itemLoader.loadHistory();
+        }
+        else window.alert('cart is empty!')
+    }
+
+    itemLoader.loadHistory = async function () {
+        let authToken = checkToken()
+        if (!authToken) props.auth()
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Authorization", authToken);
+
+        // var raw = "";
+
+        var requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            // body: raw,
+            redirect: 'follow'
+        };
+
+        fetch("https://e-commerce.urownsite.xyz/orders", requestOptions)
+            .then(historyItems => historyItems.json())
+            .then(historyItems => {
+                console.log(historyItems);
+                if (historyItems["status"] == "Success") {
+                    historyItems = historyItems.data
+                    historyItems = historyItems.map((item) => {
+                        return { ...item, ...item.product }
+                    })
+                    console.log(historyItems, 'opopop');
+                    historyItems.push(cancellOrder)
+                    setItems(historyItems);
+                }
+                else {
+                    props.auth()
+                }
+            }).catch(error => console.log('error', error));
+    }
+        //setTimeout(()=>loadAllItems(), 1000)
+        ;
+    return (
+        <div className="App">
+            {/* <Header />
+      <Login anchorButton="buy" action="log In" /> */}
+            <Header action={items.length > 0 ? items[items.length - 1] : addToCart} itemLoader={itemLoader} auth={() => { props.auth() }} />
+            <Body items={items} itemLoader={itemLoader} action={items.length > 0 ? items[items.length - 1] : addToCart} auth={() => { props.auth() }} />
+            <Footer auth={() => { props.auth() }} />
+        </div>
+    );
 }
+
+function checkToken() {
+    // console.log(localStorage.getItem('user')?JSON.parse(localStorage.getItem('user')):false)
+    return localStorage.getItem('user') && JSON.parse(localStorage.getItem('user')).authorization ? JSON.parse(localStorage.getItem('user')).authorization : false
+}
+
 
 export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import imageSample from "../images/p1.png"
+
+export default function ImgMediaCard(props) {
+  console.log(props.action)
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image={imageSample}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        {props.item.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {props.item.description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" onClick={()=>{props.action.action(props.item)}}>{props.action.name}</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
