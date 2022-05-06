24e46fbeb3a5de16cf509e4b1f45561168211a6c
Shubham Shrikant Bawner <wurtzreaxnwittigreaxn@gmail.com>
2022-05-06 21:56:16 +0530

    revert

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -126,6 +126,7 @@ export default function DashboardHeader(props) {
                 >
                     <AutocompleteSearch items={props.items} itemLoader={props.itemLoader} searchName={searchName} />
 
+                    
                     <IconButton onClick={toggleDrawer}>
                         <ChevronLeftIcon />
                     </IconButton>
@@ -235,6 +236,10 @@ export default function DashboardHeader(props) {
                     >
                         <SearchIcon />
                     </IconButton>
+                    
+                    <div onClick={razorPay}>payments!</div>
+                    
+                    
 
                     {props.toggleElement}
 
@@ -251,10 +256,50 @@ export default function DashboardHeader(props) {
             />
 
 
+                    <div onClick={razorPay}>payments!</div>
 
         </Box>
 
     );
+}
+
+function razorPay(props){
+    var options = {
+        "key": "rzp_test_sgJ0mLdehTD2jF", 
+        "amount": "500", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+        "currency": "INR",
+        "name": "Acme Corp",
+        "description": "Test Transaction",
+        "image": "https://example.com/your_logo",
+        // "order_id": "order_IluGWxBm9U8zJ8", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+        "handler": function (response){
+            alert(response.razorpay_payment_id);
+            alert(response.razorpay_order_id);
+            alert(response.razorpay_signature)
+        },
+        "prefill": {
+            "name": "Gaurav Kumar",
+            "email": "gaurav.kumar@example.com",
+            "contact": "9999999999"
+        },
+        "notes": {
+            "address": "Razorpay Corporate Office"
+        },
+        "theme": {
+            "color": "#3399cc"
+        }
+    };
+    var rzp1 = new Razorpay(options);
+    rzp1.on('payment.failed', function (response){
+            alert(response.error.code);
+            alert(response.error.description);
+            alert(response.error.source);
+            alert(response.error.step);
+            alert(response.error.reason);
+            alert(response.error.metadata.order_id);
+            alert(response.error.metadata.payment_id);
+    });
+    rzp1.open();
 }
 
 function AutocompleteSearch(props) {
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,8 @@ function Copyright() {
       </Link>{' '}
       <Link color="inherit" href="https://www.linkedin.com/in/shubham-bawner-456131205/">
         LinkedIn
-      </Link>
+      </Link><br/>{' '}
+      
       
     </Typography>
   );
@@ -43,7 +44,64 @@ export default function StickyFooter(props) {
           </Typography>
           <Copyright />
         </Container>
+        <Container  maxWidth="sm">
+        <Link color="inherit" href="https://www.linkedin.com/in/shubham-bawner-456131205/">
+      About Us
+      </Link><br/>{' '}
+      <Link color="inherit" href="https://www.linkedin.com/in/shubham-bawner-456131205/">
+      Contact Us
+      </Link><br/>{' '}
+      <Link color="inherit" href="https://www.linkedin.com/in/shubham-bawner-456131205/">
+      Privacy Policy
+      </Link><br/>{' '}
+      <Link color="inherit" href="https://www.linkedin.com/in/shubham-bawner-456131205/">
+      Terms and Conditions
+      </Link><br/>{' '}
+      <Link color="inherit" href="https://www.linkedin.com/in/shubham-bawner-456131205/">
+      Cancellation/Refund Policies
+      </Link><br/>{' '}
+        </Container>
+        <div id = "razorPay" onClick={razorPay}>payments!</div>
       </Box>
     
   );
+}
+
+function razorPay(props){
+  var options = {
+      "key": "rzp_test_sgJ0mLdehTD2jF", 
+      "amount": "500", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      "currency": "INR",
+      "name": "Acme Corp",
+      "description": "Test Transaction",
+      "image": "https://example.com/your_logo",
+      // "order_id": "order_IluGWxBm9U8zJ8", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      "handler": function (response){
+          alert(response.razorpay_payment_id);
+          alert(response.razorpay_order_id);
+          alert(response.razorpay_signature)
+      },
+      "prefill": {
+          "name": "Gaurav Kumar",
+          "email": "gaurav.kumar@example.com",
+          "contact": "9999999999"
+      },
+      "notes": {
+          "address": "Razorpay Corporate Office"
+      },
+      "theme": {
+          "color": "#3399cc"
+      }
+  };
+  var rzp1 = new Razorpay(options);
+  rzp1.on('payment.failed', function (response){
+          alert(response.error.code);
+          alert(response.error.description);
+          alert(response.error.source);
+          alert(response.error.step);
+          alert(response.error.reason);
+          alert(response.error.metadata.order_id);
+          alert(response.error.metadata.payment_id);
+  });
+  rzp1.open();
 }
